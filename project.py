13206c62856d7e58311e0ffd511a67df131a2baa
abetare1 <abhinav.tare1@gmail.com>
2026-04-23 16:52:46 -0400

    Add function to get a valid guess from the user

--- project.py
+++ project.py
@@ -1,5 +1,14 @@
 import random
 
+def get_guess():
+    user_input = input("Enter your guess: ")
+
+    while not user_input.isdigit():
+        print("Invalid input, please enter a number.")
+        user_input = input("Enter your guess: ")
+
+    return int(user_input)
+
 def play_game():
     number = random.randint(1,100)
     attemps = 0
