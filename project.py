96f4c19391885f01e8930d19241564afef46e505
abetare1 <abhinav.tare1@gmail.com>
2026-04-23 22:35:04 -0400

    Updating play_game() to check users guesses

--- project.py
+++ project.py
@@ -13,6 +13,12 @@ def play_game():
     number = random.randint(1,100)
     attemps = 0
     guessed_correctly = False
+    guesses = []
 
     print("Welcome to the Number Guessing Game!")
     print("I am thinking of a number between 1 and 100.")
+
+    while not guessed_correctly:
+        guess = get_guess()
+        guesses.append(guess)
+        attemps += 1
